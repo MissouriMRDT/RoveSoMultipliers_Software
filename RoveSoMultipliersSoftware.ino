c7e7eca37276ec5aa340c9dee290ef3bf17c4287
Eli Byrd <Eli@thebyrdnest.net>
2023-09-13 22:15:14 -0500

    Add files via upload
    Add file sent by Arno in Discord

---
+++ RoveSoMultipliersSoftware.ino
@@ -0,0 +1,35 @@
+#include <XInput.h>
+
+float outputs[8]; // Declare the global outputs array
+int inputs[8] = {A0, A1, A2, A3, A7, A8, A9, A10};
+
+void setup() {
+  XInput.begin();
+
+  for (int i = 0; i < 8; i++) { //Set Pins
+    pinMode(inputs[i], INPUT);
+  }
+}
+
+void loop() {
+  readSliders(); // Call the readSliders function to update the outputs array
+  sendJoysticks(); // Call the sendjoysticks function to send joystick values to the device
+  XInput.send();
+}
+
+void sendJoysticks() { // Ouputs the slider values as joystick postions
+  XInput.press(BUTTON_Y);
+  XInput.setJoystick(JOY_LEFT, outputs[0], outputs[1]);
+  XInput.setJoystick(JOY_RIGHT, outputs[2], outputs[3]);
+  delay(50);
+  XInput.release(BUTTON_Y);
+  XInput.setJoystick(JOY_LEFT, outputs[4], outputs[5]);
+  XInput.setJoystick(JOY_RIGHT, outputs[6], outputs[7]);
+  delay(50);
+}
+
+void readSliders() { // Reads the values of the sliders
+  for (int i = 0; i < 8; i++) {
+    outputs[i] = (analogRead(inputs[i]) * 2 / 1023.0 - 1) * 32767;
+  }
+}
